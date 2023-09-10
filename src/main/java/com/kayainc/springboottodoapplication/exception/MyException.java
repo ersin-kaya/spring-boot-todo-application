3bb1a7449bfcd0fd53bdbe4a1dc9deb3359c59c4
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-09-10 18:37:34 +0300

    Exceptions added

---
+++ src/main/java/com/kayainc/springboottodoapplication/exception/MyException.java
@@ -0,0 +1,7 @@
+package com.kayainc.springboottodoapplication.exception;
+
+public class MyException extends RuntimeException {
+    public MyException(String message) {
+        super(message);
+    }
+}
