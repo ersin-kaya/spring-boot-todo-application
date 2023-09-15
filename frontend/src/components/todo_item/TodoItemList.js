8dec54d3ecccacdd3d84c326739ea89df0f19219
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-09-15 13:28:56 +0300

    TodoItemList component's first view

--- frontend/src/components/todo_item/TodoItemList.js
+++ frontend/src/components/todo_item/TodoItemList.js
@@ -1,6 +1,122 @@
 // rfc
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { withTranslation } from "react-i18next";
+import TodoItemService from "../../services/TodoItemService";
+import { Link } from "react-router-dom";
 
-export default function TodoItemList() {
-  return <div>TodoItemList</div>;
+function TodoItemList({ t, i18n, props }) {
+  // Redirect
+  let navigate = useNavigate();
+
+  // State
+  const [TodoItemStateService, setTodoItemStateService] = useState([]);
+
+  // UseEffect
+  useEffect(() => {
+    TodoItemService.getAll()
+      .then((response) => {
+        // console.log(response.data);
+        setTodoItemStateService(response.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  // GetAll
+  const getAll = () => {
+    TodoItemService.getAll()
+      .then((response) => {
+        // console.log(response.data);
+        setTodoItemStateService(response.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  // GetById
+  const getById = (id) => {
+    localStorage.setItem("todoItem_update_id", id);
+  };
+
+  // Update
+  const updateTodoItem = (data) => {
+    let { id, description, complete, createdDate, modifiedDate } = data;
+    localStorage.setItem("todoItem_update_id", id);
+    localStorage.setItem("todoItem_update_description", description);
+    localStorage.setItem("todoItem_update_complete", complete);
+    localStorage.setItem("todoItem_update_created_date", createdDate);
+    localStorage.setItem("todoItem_update_modified_date", modifiedDate);
+  };
+
+  // Delete
+  const deleteTodoItem = (id) => {
+    if (window.confirm("Want to delete?")) {
+      TodoItemService.delete(id).then(() => {
+        getAll();
+      });
+    } else {
+      alert("Couldn't delete this task.");
+    }
+    navigate("/todoItem/list");
+  };
+
+  return (
+    <div>
+      {/* <Link to="todoItem/create" className="btn btn-primary"></Link> */}
+      <table className="table table-striped table-hover table-responsive">
+        <thead>
+          <tr>
+            <th>{t("ID")}</th>
+            <th>{t("Description")}</th>
+            <th>{t("Created Date")}</th>
+            <th>{t("Modified Date")}</th>
+            <th>{t("Complete")}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {TodoItemStateService.map((data) => (
+            <tr key={data.id}>
+              <td>{data.id}</td>
+              <td>{data.description}</td>
+              <td>{data.createdDate}</td>
+              <td>{data.modifiedDate}</td>
+              <td>{data.complete.toString()}</td>
+
+              <td>
+                <Link to={`/todoItem/update/${data.id}`}>
+                  <i
+                    onClick={() => updateTodoItem(data)}
+                    className="fa-solid fa-pen-to-square text-primary"
+                  ></i>
+                </Link>
+              </td>
+
+              <td>
+                <Link to={`/todoItem/detail/${data.id}`}>
+                  <i
+                    onClick={() => getById(data.id)}
+                    className="fa-solid fa-expand text-warning"
+                  ></i>
+                </Link>
+              </td>
+
+              <td>
+                <Link to={`/todoItem/delete}`}>
+                  <i
+                    onClick={() => deleteTodoItem(data.id)}
+                    className="fa-solid fa-trash text-danger"
+                  ></i>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
+
+export default withTranslation()(TodoItemList);
