e33a45354d13de3c1ad110d4f92adc06e9dbafa8
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-09-15 11:18:39 +0300

    Tags with examples

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+// import notebook from "../images/notebook.jpg";
 
 export default class Main extends Component {
   render() {
-    return <div>Main</div>;
+    return (
+      <div>
+        {/* <h1>To Do List</h1> */}
+        {/* <Link className="btn btn-primary" to="todoItem/list">
+          Button
+        </Link> */}
+        {/* <img src={notebook} alt="" /> */}
+      </div>
+    );
   }
 }
