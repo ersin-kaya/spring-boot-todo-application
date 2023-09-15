33c972929ba2d6316194db96d39b85753ac1ddfd
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-09-15 16:27:08 +0300

    TodoItemUpdate component's first view

--- frontend/src/components/todo_item/TodoItemUpdate.jsx
+++ frontend/src/components/todo_item/TodoItemUpdate.jsx
@@ -1,5 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import TodoItemService from "../../services/TodoItemService";
+import { withTranslation } from "react-i18next";
 
-export default function TodoItemUpdate() {
-  return <div>TodoItemUpdate</div>;
+function TodoItemUpdate({ t }) {
+  // Redirect
+  let navigate = useNavigate();
+
+  // STATE
+  const [todoItem, setTodoItem] = useState("");
+  const [checked, setChecked] = React.useState(false);
+  const [id, setId] = useState();
+
+  const todoItemData = useParams();
+
+  useEffect(() => {
+    setId(todoItemData.id);
+    TodoItemService.getById(todoItemData.id)
+      .then((response) => {
+        setTodoItem(response.data.description);
+        setChecked(response.data.complete);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  const handleChange = () => {
+    setChecked(!checked);
+  };
+
+  const todoItemUpdate = async (event) => {
+    event.preventDefault();
+
+    const newTodoItem = JSON.parse(
+      `{"description":"${todoItem}", "complete":${checked}}`
+    );
+    // console.log(newTodoItem);
+
+    try {
+      const response = await TodoItemService.update(id, newTodoItem);
+      if (response.status === 200) {
+        navigate("/todoItem/list");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <form>
+        <h2 className="display-3 mt-5">{t("Task update")}</h2>
+        <div className="form-group mt-4">
+          <input
+            type="text"
+            className="form-control"
+            placeholder={t("Description")}
+            required={true}
+            autoFocus={true}
+            id="todo_description"
+            name="todo_description"
+            onChange={(event) => {
+              setTodoItem(event.target.value);
+            }}
+            value={todoItem}
+          />
+          <div className="form-check mt-3">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              checked={checked}
+              onChange={handleChange}
+            />
+            <label className="form-check-label" htmlFor="">
+              Status
+            </label>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary mt-3 mb-5"
+          onClick={todoItemUpdate}
+        >
+          {t("update")}
+        </button>
+      </form>
+    </div>
+  );
 }
+
+export default withTranslation()(TodoItemUpdate);
