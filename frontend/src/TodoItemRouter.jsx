ca468ccd0f21b7517bbd5c72c66f3c4b2af698b2
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-09-15 10:00:34 +0300

    TodoItemRouter.jsx - Compiled with problems

--- frontend/src/TodoItemRouter.jsx
+++ frontend/src/TodoItemRouter.jsx
@@ -1,8 +1,16 @@
 // rcc
 import React, { Component } from "react";
+
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+
+import { TodoItemList } from "./components/todo_item/TodoItemList";
+import { TodoItemCreate } from "./components/todo_item/TodoItemCreate";
+import { TodoItemUpdate } from "./components/todo_item/TodoItemUpdate";
+// import {TodoItemView} from "./components/todo_item/TodoItemView";
+
+import { Routes, Route } from "react-router-dom";
 
 export default class TodoItemRouter extends Component {
   static displayName = "TodoItem_Router";
@@ -18,7 +26,19 @@ export default class TodoItemRouter extends Component {
     return (
       <div>
         <Header bars="fa-solid fa-bars" />
-        <Main />
+
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/todoItem/list" element={TodoItemList} />
+            <Route path="/todoItem/create" element={TodoItemCreate} />
+            <Route path="/todoItem/udpate:id" element={TodoItemUpdate} />
+            {/* <Route path="/todoItem/view/:id" element={TodoItemView} /> */}
+
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+
         <Footer copyright="fa-regular fa-copyright" />
       </div>
     );
